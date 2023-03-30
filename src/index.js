4e8616091aa99e9be9365d89cd8116eb0d0375c3
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-03-30 23:38:57 +0200

    added libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.getElementById('search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('country-info');
+
+
